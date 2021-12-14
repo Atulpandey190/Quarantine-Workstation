685b5873e872d8b5e03e364eb402d9db7616c387
Killshift <kshitijsharma8091@gmail.com>
2021-12-15 00:18:20 +0530

    first commit

--- src/Controlarea.jsx
+++ src/Controlarea.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMicrophone } from '@fortawesome/free-solid-svg-icons'
-import { faMicrophoneSlash } from '@fortawesome/free-solid-svg-icons'
-import { faVideo } from '@fortawesome/free-solid-svg-icons'
-import { faVideoSlash } from '@fortawesome/free-solid-svg-icons'
+import './Controlarea.css'
+import { MicFill,MicMuteFill,GearFill,CameraVideoFill,CameraVideoOffFill } from 'react-bootstrap-icons';
 export default function Controlarea() {
     return (
         <div className='control-area'>
             <h1>This is the control area</h1>
             <div className='icons-area'>
-            <FontAwesomeIcon  icon={faMicrophone} className='icons'></FontAwesomeIcon>
-           {/*<FontAwesomeIcon className='icons' icon={faMicrophoneSlash}></FontAwesomeIcon>*/}
-           <FontAwesomeIcon  icon={faVideo} className='icon'></FontAwesomeIcon>
-           {/*<FontAwesomeIcon className='icons' icon={faVideoSlash}></FontAwesomeIcon>*/}
+             <MicFill className="icon first" size={25}/>
+             {/*<MicMuteFill size={50}/>*/}
+             <CameraVideoFill className="icon second" size={25}/>
+             {/*<CameraVideoOffFill size={50}/>*/}
+             <GearFill className="icon third"size={25}/>
+
             </div>
 
         </div>
