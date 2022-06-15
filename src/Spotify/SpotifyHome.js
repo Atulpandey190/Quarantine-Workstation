49924eafd661209ae4d20c0c03fae377ec63354d
Shreyas Pandey <shrey.devep@gmail.com>
2022-06-15 13:40:10 +0530

    Minor Changes

--- src/Spotify/SpotifyHome.js
+++ src/Spotify/SpotifyHome.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
-import "../Home/Home.css";
-
+import { useDispatch, useSelector } from "react-redux";
+import "../Spotify/SpotifyHome.css";
+import { setShowMusicPlayer } from "../store/actions/dashboardActions";
 export default function SpotifyHome({ AUTH_URL }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const skipHandler = () => {
+    dispatch(setShowMusicPlayer(false));
+    navigate("/home");
+  };
   return (
-    <div className="home">
-      <div className="login">
-        <div className="login-area">
-          <a className="login-btn" href={AUTH_URL}>
+    <div className="home-spotify">
+      <div className="login-spotify">
+        <div className="login-area-spotify">
+          <a className="login-btn-spotify" href={AUTH_URL}>
             Login Spotify
           </a>
+          <button className="spotify-btn" onClick={skipHandler}>
+            <i className="fas fa-forward" style={{ color: "white" }}></i>
+          </button>
         </div>
       </div>
     </div>
